d3818a2d890dbdae4aed8e36ddf00c419fa63fc1
rajgaria7 <u1471428@gcloud.utah.edu>
2024-09-01 17:32:17 -0600

    added disease data experiments

---
+++ disease_thor_experiments/constants.py
@@ -0,0 +1,11 @@
+custom_cache_dir = "/scratch/general/vast/u1471428/mapqa_data/models_cache"
+
+qa_disease_precaution = "disease_QA_precaution.json"
+qa_disease_symptom = "disease_QA_symptoms.json"
+
+
+train_disease_file = "train_augmented_qa.json"
+test_disease_file = "test_augmented_qa.json"
+
+
+model_save_dir = "/scratch/general/vast/u1471428/dcp/models"
